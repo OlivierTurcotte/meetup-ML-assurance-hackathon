e1bef11b1573b129fc82562bdf181bd3e3809436
Olivier Turcotte <olivier.turcotte.18@ulaval.ca>
2018-11-10 09:17:38 -0500

    Importing datas

--- src/script.R
+++ src/script.R
@@ -1,1 +1,7 @@
 ################## Ceci est l'art de Guillaume Michel et Olivier Turcotte ###############
+
+if(!require("pacman")) install.packages("pacman")
+pacman::p_load(data.table)
+
+data_train_features <- fread("data/train-features.csv")
+data_train_reponses <- fread("data/data-id-train.csv")
